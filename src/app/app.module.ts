5f1192df7cd5c53ef85b91274da73ff09dfc0cf0
Alby2901 <a.azzola@scriptanet.it>
2022-09-25 20:43:16 +0200

    20220925-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { RecipelistComponent } from './recipes/recipelist/recipelist.component';
+import { RecipeitemComponent } from './recipes/recipelist/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './recipes/recipedetail/recipedetail.component';
+import { ShoppinglisteditComponent } from './shoppinglist/shoppinglistedit/shoppinglistedit.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppinglistComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipedetailComponent,
+    ShoppinglisteditComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
