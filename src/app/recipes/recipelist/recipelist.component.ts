c79f18d421d03041f0efc9de16046a94b36e88c4
Alby2901 <a.azzola@scriptanet.it>
2022-09-28 21:44:28 +0200

    20220928-01

--- src/app/recipes/recipelist/recipelist.component.ts
+++ src/app/recipes/recipelist/recipelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipelist',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipelist.component.css']
 })
 export class RecipelistComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Spaghetti alla matriciana',
+      'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+      'https://media.istockphoto.com/photos/bucatini-allamatriciana-wikipedia-the-free-encyclopedia-picture-id1264712750?s=612x612'),
+    new Recipe('Risotto alla milanese', 
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      'https://media.istockphoto.com/photos/risotto-picture-id509486128?k=20&m=509486128&s=612x612&w=0&h=pkvM-GnAG0rLEZeqLisj-9VHvtlLVuYM4AKdbdsHX1M=')
+  ];
   constructor() { }
 
   ngOnInit(): void {
